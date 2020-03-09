4494f60f24c1b8bad1f2ec52f5dc3924794d893c
Igor-Kuz <dragondusty@gmail.com>
2020-03-09 14:43:23 +0300

    added settings for heroku

--- website/settings.py
+++ website/settings.py
@@ -1,5 +1,7 @@
 import os
-
+import django_heroku
+import dj_database_url
+from decouple import config 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -112,3 +115,7 @@ STATIC_URL = '/static/'
 STTICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
